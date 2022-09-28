e186611ab6f127338dc6916272bcd49650ff1bef
ahmad <bha296780@gmail.com>
2022-09-28 16:06:57 -0400

    final

---
+++ InterfaceExercise/Car.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceExercise
+{
+    public interface IVehicle : ICompany
+    {
+        int numberOfWheels { get; set; }
+        int numberOfDoors { get; set; }
+        int numberOfWindows { get; set; }
+        int numberOfSeats { get; set; }
+
+    }
+    public interface ICompany
+    {
+        string Logo { get; set; }
+        string MadeIn { get; set; }
+
+    }
+
+    public class Car : IVehicle
+    {
+        public bool HasTrunk { get; set; }
+        public bool sedan { get; set; }
+        public string Logo { get; set; }
+        public string MadeIn { get; set; }
+        public int numberOfWheels { get; set; }
+        public int numberOfDoors { get; set; }
+        public int numberOfWindows { get; set; }
+        public int numberOfSeats { get; set; }
+
+
+    }
+    public class Truck : IVehicle
+    {
+        public bool HasOpenTrunk { get; set; }
+        public bool truck { get; set; }
+        public int numberOfWheels { get; set; }
+        public int numberOfDoors { get; set; }
+        public int numberOfWindows { get; set; }
+        public int numberOfSeats { get; set; }
+        public string Logo { get; set; }
+        public string MadeIn { get; set; }
+    }
+    public class Suv :  IVehicle
+    {
+        public bool suv { get; set; }
+        public bool Has7Seats { get; set; }
+
+        public int numberOfWheels { get; set; }
+        public int numberOfDoors { get; set; }
+        public int numberOfWindows { get; set; }
+        public int numberOfSeats { get; set; }
+        public string Logo { get; set; }
+        public string MadeIn { get; set; }
+    }
+}
--- InterfaceExercise/Program.cs
+++ InterfaceExercise/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace InterfaceExercise
 {
+
     class Program
     {
+
         static void Main(string[] args)
         {
             //TODO Be sure to follow BEST PRACTICES when creating classes and interfaces
@@ -13,32 +16,82 @@ namespace InterfaceExercise
             //Create 3 classes called Car , Truck , & SUV
 
             //In your IVehicle
-            
-                /* Create 4 members that Car, Truck, & SUV all have in common.
-                 * Example: All vehicles have a number of wheels... for now..
-                 */
-            
+
+            /* Create 4 members that Car, Truck, & SUV all have in common.
+             * Example: All vehicles have a number of wheels... for now..
+             */
+
 
             //In ICompany
-            
-                /*Create 2 members that are specific to each every company
-                 * regardless of vehicle type.
-                 *
-                 *
-                 * Example: public string Logo { get; set; }
-                 */
+
+            /*Create 2 members that are specific to each every company
+             * regardless of vehicle type.
+             *
+             *
+             * Example: public string Logo { get; set; }
+             */
 
             //In each of your car, truck, and suv classes
 
-                /*Create 2 members that are specific to each class
-                 * Example: truck has a bed size while car has a trunk while suv has a cargo hold size
-                 *
-                 * Then, Set each class to inherit from both IVehicle and ICompany and implement their members.
-                 * 
-                 */
+            /*Create 2 members that are specific to each class
+             * Example: truck has a bed size while car has a trunk while suv has a cargo hold size
+             *
+             * Then, Set each class to inherit from both IVehicle and ICompany and implement their members.
+             * 
+             */
 
             //Now, create objects of your 3 classes and give their members values;
             //Creatively display and organize their values
+            Car honda = new Car()
+            {
+                HasTrunk = true,
+                sedan = true,
+                Logo = "H",
+                MadeIn = "Japan",
+                numberOfDoors = 4,
+                numberOfSeats = 5,
+                numberOfWheels = 4,
+                numberOfWindows = 4
+            };
+
+
+            Truck ford = new Truck()
+            {
+                numberOfWindows = 4,
+                numberOfWheels = 6,
+                numberOfSeats = 4,
+                numberOfDoors = 4,
+                MadeIn = "America",
+                Logo = "ford",
+                HasOpenTrunk = true,
+                truck = true
+            };
+
+            Suv esclade = new Suv()
+            {
+                Logo = "cadilac",
+                MadeIn = "america",
+                numberOfDoors = 4,
+                numberOfSeats = 7,
+                numberOfWheels = 4,
+                numberOfWindows = 6,
+                Has7Seats = true,
+                suv = true
+            };
+            List<IVehicle> list = new List<IVehicle>();
+            list.Add(honda);
+            list.Add(ford);
+            list.Add(esclade);
+            foreach (var item in list)
+            {
+                Console.WriteLine($"{item.MadeIn}, " +
+                    $"{item.numberOfWindows} ," +
+                    $"{item.numberOfWheels}, " +
+                    $"{item.Logo},{item.numberOfSeats}, " +
+                    $"{item.numberOfDoors}");
+
+            }
+
         }
     }
 }
